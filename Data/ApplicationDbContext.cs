c974d9d81df7ff6553d6505c6e5e100afbfeb3f8
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-05-15 20:40:35 -0500

    ApplicationUserZone

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace capstone.Data
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
         public IConfiguration Configuration { get; }
+        public DbSet<ApplicationUserZone> ApplicationUserZones {get;}
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
---
+++ Data/Migrations/20200516010737_ApplicationUserZone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace capstone.Data.Migrations
+{
+    public partial class ApplicationUserZone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApplicationUserZones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ZoneId = table.Column<string>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    Visable = table.Column<bool>(nullable: false),
+                    UserId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUserZones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationUserZones_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationUserZones_UserId",
+                table: "ApplicationUserZones",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationUserZones");
+        }
+    }
+}
