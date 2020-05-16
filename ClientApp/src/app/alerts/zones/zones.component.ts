de59770942c208fff75777959e9a198829104f0c
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-05-15 19:41:35 -0500

    table formatting

--- ClientApp/src/app/alerts/zones/zones.component.ts
+++ ClientApp/src/app/alerts/zones/zones.component.ts
@@ -38,7 +38,7 @@ export class ZonesComponent implements OnInit {
       .pipe(
         startWith(''),
         map(value => typeof value === 'string' ? value : value.caption),
-        map(caption => caption ? this._filter(caption) : this.states.slice())
+        map(caption => caption ? this.FilterState(caption) : this.states.slice())
       );
   }
 
@@ -61,7 +61,7 @@ export class ZonesComponent implements OnInit {
       });
   }
 
-  private _filter(caption: string): any[] {
+  private FilterState(caption: string): any[] {
     return this.states
       .filter(option => option.caption
         .toLowerCase()
