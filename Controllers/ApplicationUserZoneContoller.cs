5701e0e97aadc13b96277e2775a339f69773dd45
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-05-18 08:22:31 -0500

    controller

---
+++ Controllers/ApplicationUserZoneContoller.cs
@@ -0,0 +1,20 @@
+﻿using capstone.Data;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace capstone.Controllers
+{
+    [Route("[controller]")]
+    public class ApplicationUserZoneController : Controller
+    {
+        // learn how to use this
+        // private readonly ILogger<OidcConfigurationController> logger;
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserZoneController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
