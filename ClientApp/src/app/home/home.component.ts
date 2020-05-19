c89ec7f2252a2b5f2a18aadf3d2aa98fce27556b
jordan.woodruff <Jordan.Woodruff@tylertech.com>
2020-05-18 21:01:13 -0500

    date examples

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -8,6 +8,10 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent {
 
+  // this has a 'Z', angular date pipe will treat it correct;
+  date1: Date = new Date('2020-05-19T00:21:09.636Z');
+  // this does not have a 'Z', but because it was localy angular will still treat it correctly;
+  date2: Date = new Date('2020-05-18T20:28:40.933');
   user: Observable<IUser>;
   constructor(private authService: AuthorizeService) {
     this.user = this.authService.getUser();
