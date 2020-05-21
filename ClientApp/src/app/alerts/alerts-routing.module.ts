be1fd596ab47c3431ed2bf8a73910bc47441a4a8
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-05-20 21:52:25 -0500

    no alerts!

--- ClientApp/src/app/alerts/alerts-routing.module.ts
+++ ClientApp/src/app/alerts/alerts-routing.module.ts
@@ -4,13 +4,14 @@ import { AlertsComponent } from './alerts/alerts.component';
 import { AlertsViewComponent } from './alerts-view/alerts-view.component';
 import { ZonesComponent } from './zones/zones.component';
 import { MyZonesComponent } from './my-zones/my-zones.component';
+import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 
 
 const routes: Routes = [
   { path: 'alerts/:id', component: AlertsViewComponent },
   { path: 'alerts', component: AlertsComponent },
   { path: 'zones', component: ZonesComponent },
-  { path: 'myzones', component: MyZonesComponent }
+  { path: 'myzones', component: MyZonesComponent, canActivate: [AuthorizeGuard] }
 ];
 
 @NgModule({
