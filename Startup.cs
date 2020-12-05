e745c9376d96ef17c7d1659d81eccb50de1a39af
jordan.woodruff <jordan.woodruff@tylertech.com>
2020-12-05 14:23:42 -0600

    require confirmed account

--- Startup.cs
+++ Startup.cs
@@ -29,7 +29,7 @@ namespace capstone
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>()
+            services.AddDefaultIdentity<ApplicationUser>( options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
