6850850505a154347b59ccadfa3d7de6d105c9d5
Jordan <boogermanus@gmail.com>
2024-06-04 22:06:06 -0500

    rest sharp

---
+++ WeatherAlertsApi/WeatherAlertsApi/Controllers/ZoneController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using WeatherAlertsApi.Core.ApiModels;
+
+namespace WeatherAlertsApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ZoneController : Controller
+{
+
+}
