e742e4d5b9877a74b0bc48e802cbfb72f26d0e8a
Jordan <boogermanus@gmail.com>
2024-06-09 14:46:30 -0500

    remove angular again

--- WeatherAlerts/src/app/app-routing.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
-
-@NgModule({
-    declarations: [],
-    imports: [RouterModule.forRoot([])],
-    exports: [AppRoutingModel]
-})
-export class AppRoutingModel {}
