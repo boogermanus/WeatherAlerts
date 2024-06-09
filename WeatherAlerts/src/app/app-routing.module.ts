1a84ba42855486751abbf5aeb5336c1d77c02e7f
Jordan <boogermanus@gmail.com>
2024-06-09 14:44:39 -0500

    new component and module

---
+++ WeatherAlerts/src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot([])],
+    exports: [AppRoutingModel]
+})
+export class AppRoutingModel {}
