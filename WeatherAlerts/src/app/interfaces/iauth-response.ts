de23e6ebb4cdd475673f750c7c0fe52c0e4b82ba
Jordan <boogermanus@gmail.com>
2024-06-13 17:56:02 -0500

    new package, and auth layout

---
+++ WeatherAlerts/src/app/interfaces/iauth-response.ts
@@ -0,0 +1,4 @@
+export interface IAuthResponse {
+    access_token: string;
+    access_type: string;
+}
--- WeatherAlerts/src/app/services/auth.service.ts
+++ WeatherAlerts/src/app/services/auth.service.ts
@@ -1,11 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthModel } from '../models/auth-model';
+import { Observable } from 'rxjs';
+import { IAuthResponse } from '../interfaces/iauth-response';
+import { ApiConfig } from '../config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private readonly httpClient: HttpClient) { }
+  constructor(
+    private readonly httpClient: HttpClient,
+    private readonly jwtService: JwtHelperService) { }
+
+    public login(model: AuthModel): Observable<IAuthResponse> {
+      return this.httpClient.post<IAuthResponse>(`${ApiConfig.authApi}/login`,model);
+    }
 
 }
