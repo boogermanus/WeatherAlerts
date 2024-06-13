ddab0d583f50aaef551b59b484aaf4dc545d0d67
Jordan <boogermanus@gmail.com>
2024-06-13 16:30:30 -0500

    more fun with styles and bootstrap

--- WeatherAlerts/src/app/auth/login/login.component.ts
+++ WeatherAlerts/src/app/auth/login/login.component.ts
@@ -1,17 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,RouterModule],
+  imports: [FormsModule,RouterModule,ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit {
 
-  private formLogin: FormGroup
+  public formLogin: FormGroup
 
   constructor(
     private readonly formBuilder: FormBuilder,
@@ -25,6 +26,14 @@ export class LoginComponent implements OnInit {
     }
   ngOnInit(): void {
     
+  }
+
+  public submit() : void {}
+  public register() : void {}
+
+  public getError(pControlName: string) {
+    return this.formLogin.controls[pControlName].touched
+    && this.formLogin.controls[pControlName].hasError('required');
   }
 
 }
