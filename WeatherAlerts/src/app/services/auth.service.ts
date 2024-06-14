d8cc0b6a16fe3250d0abc2ff90531fdca93efea9
Jordan <boogermanus@gmail.com>
2024-06-14 17:57:10 -0500

    auth service

--- WeatherAlerts/src/app/services/auth.service.ts
+++ WeatherAlerts/src/app/services/auth.service.ts
@@ -11,12 +11,29 @@ import { ApiConfig } from '../config';
 })
 export class AuthService {
 
+  private readonly TOKEN: string="token";
   constructor(
     private readonly httpClient: HttpClient,
     private readonly jwtService: JwtHelperService) { }
 
-    public login(model: AuthModel): Observable<IAuthResponse> {
-      return this.httpClient.post<IAuthResponse>(`${ApiConfig.authApi}/login`,model);
-    }
+  public login(model: AuthModel): Observable<IAuthResponse> {
+    return this.httpClient.post<IAuthResponse>(`${ApiConfig.authApi}/login`, model);
+  }
+
+  public authenticate(token: string): void {
+    localStorage.setItem(this.TOKEN, token)
+  }
+
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem(this.TOKEN);
+    // return false if token is expired.
+    return !this.jwtService.isTokenExpired(token);
+  }
+
+  public userId() : string {
+    const token = localStorage.getItem(this.TOKEN);
+    const decode: any = this.jwtService.decodeToken(token);
+    return decode.nameid;
+  }
 
 }
