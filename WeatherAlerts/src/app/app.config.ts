6214ddae06e76d381dd42970d1abea6b0fa76f38
Jordan <boogermanus@gmail.com>
2024-06-17 16:39:53 -0500

    interceptor

--- WeatherAlerts/src/app/app.config.ts
+++ WeatherAlerts/src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations'
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
+import { jwtInterceptor } from './auth/jwt-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
     provideAnimations(),
     {provide: JWT_OPTIONS, useValue: JWT_OPTIONS},
     JwtHelperService
