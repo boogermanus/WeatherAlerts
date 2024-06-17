7c380bd3324e44808db4f6b62ae4a1d6dec1ba7c
Jordan <boogermanus@gmail.com>
2024-06-17 14:18:36 -0500

    fix login page

--- WeatherAlerts/src/app/app.config.ts
+++ WeatherAlerts/src/app/app.config.ts
@@ -4,11 +4,14 @@ import { provideAnimations } from '@angular/platform-browser/animations'
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { JWT_OPTIONS, JwtHelperService } from '@auth0/angular-jwt';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
-    provideAnimations()
+    provideAnimations(),
+    {provide: JWT_OPTIONS, useValue: JWT_OPTIONS},
+    JwtHelperService
   ],
 };
