2c6c8901753fe7709d244f98b35c0154042235e9
Jordan Woodruff <boogermanus@gmail.com>
2025-03-12 13:11:47 -0500

    adding map

--- WeatherAlerts/src/app/alerts/zone-map/zone-map.component.ts
+++ WeatherAlerts/src/app/alerts/zone-map/zone-map.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { LatLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-zone-map',
   standalone: true,
-  imports: [],
+  imports: [
+    LeafletModule
+  ],
   templateUrl: './zone-map.component.html',
   styleUrl: './zone-map.component.css'
 })
 export class ZoneMapComponent {
-
+  public latLng: LatLng = new LatLng(33.67,-101.82);
+  public options: any = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Open Street Map' })
+    ],
+    zoom: 5,
+    center: this.latLng
+  }
 }
