48ee0a8860a27ae17875be7c2020ee155a7a949c
Jordan Woodruff <boogermanus@gmail.com>
2025-03-12 13:37:53 -0500

    fix test

--- WeatherAlerts/src/app/alerts/alerts/alerts.component.spec.ts
+++ WeatherAlerts/src/app/alerts/alerts/alerts.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertsComponent } from './alerts.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 describe('AlertsComponent', () => {
   let component: AlertsComponent;
@@ -8,7 +10,13 @@ describe('AlertsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AlertsComponent]
+      imports: [
+        AlertsComponent
+      ],
+      providers: [
+        provideHttpClient(),
+        provideNoopAnimations()
+      ]
     })
       .compileComponents();
 
