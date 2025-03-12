7b3a34bf8154a5af804dfbea43367f510459afa3
Jordan Woodruff <boogermanus@gmail.com>
2025-03-12 13:57:51 -0500

    fix tests

--- WeatherAlerts/src/app/alerts/my-zones/my-zones.component.spec.ts
+++ WeatherAlerts/src/app/alerts/my-zones/my-zones.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyZonesComponent } from './my-zones.component';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('MyZonesComponent', () => {
   let component: MyZonesComponent;
@@ -8,7 +9,10 @@ describe('MyZonesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MyZonesComponent]
+      imports: [MyZonesComponent],
+      providers: [
+        provideHttpClient()
+      ]
     })
     .compileComponents();
     
