faad5cf70ad657003407c25db74ce6e3cbc4d931
Jordan Woodruff <boogermanus@gmail.com>
2025-03-13 16:43:17 -0500

    map zone add working!

--- WeatherAlerts/src/app/alerts/zone-map/zone-map.component.ts
+++ WeatherAlerts/src/app/alerts/zone-map/zone-map.component.ts
@@ -1,21 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
 import { icon, LatLng, Layer, LeafletMouseEvent, marker, tileLayer } from 'leaflet';
-import { Observable } from 'rxjs';
+import { Observable, Subscribable, Subscription } from 'rxjs';
 import { PointService } from '../../services/point.service';
 import { CommonModule } from '@angular/common';
+import { UserZoneService } from '../../services/user-zone.service';
+import { IUserZone } from '../../interfaces/iuser-zone';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-zone-map',
   standalone: true,
   imports: [
     LeafletModule,
-    CommonModule
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './zone-map.component.html',
   styleUrl: './zone-map.component.css'
 })
-export class ZoneMapComponent {
+export class ZoneMapComponent implements OnInit, OnDestroy {
   public latLng: LatLng = new LatLng(33.67, -101.82);
   public markers: Layer[] = [];
   public options: any = {
@@ -25,9 +35,33 @@ export class ZoneMapComponent {
     zoom: 5,
     center: this.latLng
   }
-  public info: Observable<any>;
+  public data: any = null;
+  public userZones: IUserZone[] = [];
+  public subscriptions: Subscription = new Subscription();
+  public loading: boolean = false;
 
-  constructor(private readonly pointService: PointService) {}
+  public city: string = '';
+  public state: string = '';
+  public list: string[] = [];
+
+  constructor(
+    private readonly pointService: PointService,
+    private readonly userZoneService: UserZoneService
+  ) { }
+
+  public ngOnInit(): void {
+    this.subscriptions.add(
+      this.userZoneService.getUserZones()
+        .subscribe({
+          next: (data) => this.userZones = data
+        })
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
+
 
   public handleClick(event: LeafletMouseEvent): void {
     this.latLng = event?.latlng;
@@ -44,6 +78,43 @@ export class ZoneMapComponent {
       }
     )
     this.markers.push(newMarker);
-    this.info = this.pointService.getPoints(this.latLng);
+    this.loading = true;
+    this.subscriptions.add(
+      this.pointService.getPoints(this.latLng)
+        .subscribe({
+          next: (data) => this.handleData(data)
+        })
+    );
+  }
+
+  private handleData(data: any): void {
+    this.loading = false;
+    this.data = data;
+    this.city = data.properties.relativeLocation.properties.city;
+    this.state = data.properties.relativeLocation.properties.state;
+
+    if (data.properties.forecastZone !== '') {
+      this.addZone(data.properties.forecastZone);
+    }
+
+    if (data.properties.county !== '') {
+      this.addZone(data.properties.county);
+    }
+  }
+
+  private addZone(zone: string): void {
+    const index = zone.lastIndexOf('/');
+    const zoneId = zone.substring(index + 1, zone.length);
+    const found = this.userZones.findIndex(f => f.zoneId === zoneId) !== -1;
+
+    if (!found) {
+      this.list.push(zoneId);
+    }
+  }
+
+  public async handleAdd(value: string): Promise<void> {
+    await this.userZoneService.addUserZone({ zoneId: value, createDate: new Date(), visible: true });
+    const index = this.list.findIndex(f => f === value);
+    this.list.splice(index);
   }
 }
