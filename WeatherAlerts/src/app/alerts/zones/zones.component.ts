d4b951af783305c7ce082f81c05e3f3c44620538
Jordan Woodruff <boogermanus@gmail.com>
2025-07-01 18:12:35 -0500

    cleanup

--- WeatherAlerts/src/app/alerts/zones/zones.component.ts
+++ WeatherAlerts/src/app/alerts/zones/zones.component.ts
@@ -1,30 +1,23 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ZoneSearchComponent } from '../zone-search/zone-search.component';
-import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
-import { ZoneMapComponent } from "../zone-map/zone-map.component";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ZoneSearchComponent} from '../zone-search/zone-search.component';
+import {MatButtonToggleChange, MatButtonToggleModule} from '@angular/material/button-toggle';
+import {ZoneMapComponent} from "../zone-map/zone-map.component";
 
 @Component({
-    selector: 'app-zones',
-    templateUrl: './zones.component.html',
-    styleUrl: './zones.component.css',
-    imports: [
-        ZoneSearchComponent,
-        MatButtonToggleModule,
-        ZoneMapComponent
-    ]
+  selector: 'app-zones',
+  templateUrl: './zones.component.html',
+  styleUrl: './zones.component.css',
+  imports: [
+    ZoneSearchComponent,
+    MatButtonToggleModule,
+    ZoneMapComponent
+  ]
 })
-export class ZonesComponent implements OnInit, OnDestroy {
+export class ZonesComponent {
 
   public mode: string = 'map';
-  constructor(
-  ) { }
 
-  ngOnInit(): void {
-
-  }
-
-  ngOnDestroy(): void {
-
+  constructor() {
   }
 
   public onChange(event: MatButtonToggleChange): void {
