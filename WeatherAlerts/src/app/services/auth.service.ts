06a3537a3191e482ecd0b5b32542c763bbbd2533
Jordan Woodruff <boogermanus@gmail.com>
2025-07-03 13:56:26 -0500

    fix #30 access token storage

--- WeatherAlerts/src/app/services/auth.service.ts
+++ WeatherAlerts/src/app/services/auth.service.ts
@@ -1,26 +1,27 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { AuthModel } from '../models/auth-model';
-import { BehaviorSubject, Observable, map, of, pipe, switchMap } from 'rxjs';
-import { IAuthResponse } from '../interfaces/iauth-response';
-import { ApiConfig } from '../config';
-import { RegisterModel } from '../models/register-model';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {AuthModel} from '../models/auth-model';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {IAuthResponse} from '../interfaces/iauth-response';
+import {ApiConfig} from '../config';
+import {RegisterModel} from '../models/register-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private readonly TOKEN: string = "token";
+  private readonly TOKEN: string = "weather_alerts_token";
   private authenticated: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
   constructor(
     private readonly httpClient: HttpClient,
-    private readonly jwtService: JwtHelperService) { 
-      if(this.isValidToken) {
-        this.authenticated.next(true);
-      }
+    private readonly jwtService: JwtHelperService) {
+    if (this.isValidToken) {
+      this.authenticated.next(true);
     }
+  }
 
   public login(model: AuthModel): Observable<IAuthResponse> {
     return this.httpClient.post<IAuthResponse>(`${ApiConfig.authApi}/login`, model);
