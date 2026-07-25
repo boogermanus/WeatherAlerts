f64feb5b13c6997719bfe94982ef714584483130
Jordan Woodruff <boogermanus@gmail.com>
2026-07-24 19:32:14 -0500

    Dotnet10 (#35)
    * update WeatherAlertsApi net10
    
    * fix swagger gen
    
    * refactor startup

---
+++ WeatherAlertsApi/WeatherAlertsApi/Extensions/RegisterServiceExtensions.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using WeatherAlertsApi.Infrastrcture;
+using WeatherAlertsApi.Core.Models;
+using WeatherAlertsApi.Infrastrcture.Repositories;
+using WeatherAlertsApi.Core.Interfaces.Api;
+using WeatherAlertsApi.Core.Services.Api;
+
+namespace WeatherAlertsApi.Extensions;
+
+public static class RegisterServiceExtensions
+{
+    public static IServiceCollection RegisterCommonServices(this IServiceCollection services)
+    {
+        services.AddControllers()
+            .AddNewtonsoftJson();
+        services.AddHttpContextAccessor();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Scheme = "bearer"
+            });
+            options.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+            {
+                [new OpenApiSecuritySchemeReference("bearer", document)] = []
+            });
+        });
+        return services;
+    }
+
+    public static IServiceCollection RegisterDatabaseServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ??
+            throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlite(connectionString));
+
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddDefaultIdentity<User>()
+            .AddEntityFrameworkStores<AppDbContext>();
+
+        services.AddAuthentication(options => {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"] ?? string.Empty))
+            });
+
+        return services;
+    }
+
+    public static IServiceCollection RegisterAppServices(this IServiceCollection services)
+    {
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddTransient<IUserService, UserService>();
+        services.AddScoped<IUserZoneRepository, UserZoneRepository>();
+        services.AddScoped<IUserZoneService, UserZoneService>();
+        
+        return services;
+    }
+}
--- WeatherAlertsApi/WeatherAlertsApi/Program.cs
+++ WeatherAlertsApi/WeatherAlertsApi/Program.cs
@@ -1,83 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-using WeatherAlertsApi.Infrastrcture;
-using WeatherAlertsApi.Core.Models;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using WeatherAlertsApi.Infrastrcture.Repositories;
-using Microsoft.OpenApi.Models;
-using WeatherAlertsApi.Core.Interfaces.Api;
-using WeatherAlertsApi.Core.Services.Api;
+using WeatherAlertsApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.WebHost.UseUrls("http://localhost:5004");
-builder.Services.AddControllers()
-    .AddNewtonsoftJson();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("bearer", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Scheme = "bearer"
-    });
-    options.OperationFilter<AddAuthHeaderOperationFilter>();
-});
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? 
-    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.RegisterCommonServices()
+    .RegisterDatabaseServices(builder.Configuration)
+    .RegisterAppServices();
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(connectionString));
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<User>()
-    .AddEntityFrameworkStores<AppDbContext>();
-
-builder.Services.AddAuthentication(options => {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = false,
-        ValidateAudience = false,
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? string.Empty))
-    });
-
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddTransient<IUserService, UserService>();
-builder.Services.AddScoped<IUserZoneRepository, UserZoneRepository>();
-builder.Services.AddScoped<IUserZoneService, UserZoneService>();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-//app.UseHttpsRedirection();
-app.UseCors(options => 
-{
-    options.AllowAnyOrigin();
-    options.AllowAnyHeader();
-    options.AllowAnyMethod();
-});
-
-app.UseAuthentication();
-app.UseRouting();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+builder.BuildApp().Run();
